7a580f24a9ee79b35bfd0fc406f0251af358d0b2
cms <yaahq123@gmail.com>
2018-01-22 19:46:40 +0900

    add js, css, html

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function() {
+	$("body").vegas({
+		slides : [
+			{ src : "images/bg-1.jpg"},
+			{ src : "images/bg-2.jpg"}
+		],
+		delay : 7000,
+		timer : false
+	});
+});
+
+
+$(function() {
+	$('#top_nav li').on("click", function(event) {
+		event.preventDefault();
+		var element = $(this).attr('class').split(" ")[0];
+		var destination = $('#' + element).offset().top;
+		$("html:not(:animated), body:not(:animated)").animate({
+			scrollTop : destination
+		}, 500);
+	});
+	
+	$(window).on("scroll", function() {
+		setActive($(this).scrollTop());
+	});
+});
+
+
+function setActive(offTop) {
+	var height = $(window).height();
+	var overlap = 0;
+	if(overlap == "full") {
+		overlap = height - 1;
+	}
+	var shown = Math.ceil((offTop - overlap) / height);
+	$('#top_nav li').removeClass("active").eq(shown).addClass("active");
+}
